b0157e5269a937e23ae27d5dcfc9968604b31c0d
HarryBedi <harry@bedifamily.uk>
2017-01-04 12:31:13 +0000

    test1

--- GitHubPlay/Program.cs
+++ GitHubPlay/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace GitHubPlay
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            string s = "Goodbye World";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
         }
     }
 }
